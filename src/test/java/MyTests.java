6ad27b68a9c7e978e6ce2ff1f15f477ac655d82e
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2019-07-16 17:02:45 -0400

    Prepare tests

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class MyTests {
+
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
